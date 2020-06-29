e255c3e1694f948665f110208763e14c819bd28d
eliott194 <55228898+eliott194@users.noreply.github.com>
2020-06-29 18:56:13 +0200

    Update MediaController.php

--- controller/MediaController.php
+++ controller/MediaController.php
@@ -6,11 +6,58 @@ require_once( 'model/media.php' );
 * ----- LOAD HOME PAGE -----
 ***************************/
 
+// Function allowing to go on a page when the user look on a media
 function mediaPage() {
 
-  $search = isset( $_GET['titl'] ) ? $_GET['titl'] : null;
-  $medias = Media::filterMedias( $search );
+	 if ( isset($_GET['title'])) {
 
-  require('view/mediaListView.php');
+	  $search = isset( $_GET['title'] ) ? $_GET['title'] : null;
 
+	  $medias = Media::filterMedias( $search );
+
+	  require('view/mediaListView.php');
+
+	 }
+
+	 else {
+
+	  $medias = Media::getAllMedias();
+
+	  require('view/mediaListView.php');
+	}
+}
+
+
+
+//Function allowing to go on a page when the user look on a media
+function episodePage() {
+
+	if ( isset($_GET['episode'])) {
+
+		require('view/episodeView.php');
+	}
+}
+
+
+
+// Function allowing to go on a page when the user look on a media
+function mediaSelecte() {
+
+	if ( isset($_GET['media'])) {
+
+		$value = getOne($_GET['media']);
+		$type_media = $value['type'];
+
+		if ($type_media == "series") {
+			
+			require('view/serieView.php');
+
+	 	}
+
+		if ($type_media == "movie") {
+
+	  		require('view/movieView.php');
+
+		}
+	}
 }
